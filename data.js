2f15761eaad35b60dcfb42cffecf0d5a26d7de6b
YCDStudio <ycd.studio.ops@gmail.com>
2025-12-19 14:35:30 +0300

    initial update

---
+++ data.js
@@ -0,0 +1,363 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ope_360_studio_ab",
+      "name": "OPE_360_Studio_A+B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8992126993015752,
+          "pitch": 0.10160895925329427,
+          "rotation": 0,
+          "target": "1-ope_360_studio_a"
+        },
+        {
+          "yaw": -1.271277707599264,
+          "pitch": 0.009022429572631552,
+          "rotation": 0,
+          "target": "2-ope_360_studio_b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-ope_360_studio_a",
+      "name": "OPE_360_Studio_A",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7948274728205398,
+          "pitch": 0.14240348386745438,
+          "rotation": 0,
+          "target": "0-ope_360_studio_ab"
+        },
+        {
+          "yaw": 0.30881464451109686,
+          "pitch": 0.0759832997550891,
+          "rotation": 0,
+          "target": "3-ope_360_green-room-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-ope_360_studio_b",
+      "name": "OPE_360_Studio_B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.077552079902068,
+          "pitch": 0.12440883044686935,
+          "rotation": 0,
+          "target": "0-ope_360_studio_ab"
+        },
+        {
+          "yaw": -1.5049473181821504,
+          "pitch": 0.12911662441673144,
+          "rotation": 0,
+          "target": "4-ope_360_green-room-c"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ope_360_green-room-b",
+      "name": "OPE_360_Green Room B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1686021089303438,
+          "pitch": 0.1703655930542407,
+          "rotation": 0,
+          "target": "1-ope_360_studio_a"
+        },
+        {
+          "yaw": 1.14230730027443,
+          "pitch": 0.0992852838981726,
+          "rotation": 0,
+          "target": "5-ope_360_hmu-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-ope_360_green-room-c",
+      "name": "OPE_360_Green Room C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.999425191612552,
+          "pitch": 0.1202202316553862,
+          "rotation": 0,
+          "target": "6-ope_360_hmu-c"
+        },
+        {
+          "yaw": 2.24141769186369,
+          "pitch": 0.11277405968452037,
+          "rotation": 0,
+          "target": "2-ope_360_studio_b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-ope_360_hmu-b",
+      "name": "OPE_360_HMU B",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.1702331140650077,
+          "pitch": 0.09475530514658281,
+          "rotation": 0,
+          "target": "0-ope_360_studio_ab"
+        },
+        {
+          "yaw": -1.475089494648973,
+          "pitch": 0.08363856108543999,
+          "rotation": 0,
+          "target": "3-ope_360_green-room-b"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-ope_360_hmu-c",
+      "name": "OPE_360_HMU C",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.48064893427891775,
+          "pitch": 0.019197922118760147,
+          "rotation": 0,
+          "target": "7-ope_360_vip"
+        },
+        {
+          "yaw": -2.606711932718728,
+          "pitch": 0.19540793394980938,
+          "rotation": 0,
+          "target": "0-ope_360_studio_ab"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-ope_360_vip",
+      "name": "OPE_360_VIP",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4602615693969838,
+          "pitch": 0.12305152136851483,
+          "rotation": 0,
+          "target": "6-ope_360_hmu-c"
+        },
+        {
+          "yaw": -2.4961202367901603,
+          "pitch": 0.15933033572061817,
+          "rotation": 0,
+          "target": "0-ope_360_studio_ab"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "OPE-360-TOUR",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
